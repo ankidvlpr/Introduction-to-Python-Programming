fac43c8bf11721f4354698da13c88dfe6abb9385
ankidvlpr <anki.dvlpr@gmail.com>
2026-02-01 17:50:51 +0530

    some function question practice

---
+++ 01_Basics/03_function.py
@@ -0,0 +1,13 @@
+# Please write a function named mean, which takes three integer arguments. 
+# The function should print out the arithmetic mean of the three arguments.
+
+# mean(5, 3, 1)
+# mean(10, 1, 1)
+
+
+def mean(a,b,c):
+    do = float(a+b+c)/3
+    print(do)
+if __name__ == "__main__":
+    mean(5, 3, 1)
+    mean(10, 1, 1)
--- 01_Basics/Function.py
+++ 01_Basics/Function.py
@@ -2,3 +2,11 @@ def message():
     print("This is my very own function!")
 
 message()
+
+#Function arguments
+
+def hello(target):
+    print("Hello", target)
+
+hello("Emily")
+hello("world!")
