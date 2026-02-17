c772e6a0d591f798688755cd64f14025e99fe1a9
ankidvlpr <anki.dvlpr@gmail.com>
2026-02-17 15:28:15 +0530

    learning for loops

---
+++ 02_Basics/for loops/01_loop.py
@@ -0,0 +1,26 @@
+# Please write a program which asks the user to type in a string. 
+# The program then prints each input character on a separate line.
+# After each character there should be a star (*) printed on its own line.
+
+# This is how it should work:
+
+# Sample output
+# Please type in a string: Python
+# P
+# *
+# y
+# *
+# t
+# *
+# h
+# *
+# o
+# *
+# n
+# *
+
+user = input("Please type in a string: ")
+
+for char in user:
+    print(char)
+    print("*")
