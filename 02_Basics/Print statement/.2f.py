c450abb52d375c3495ac2c14c79ce74c23f8c5ce
ankidvlpr <anki.dvlpr@gmail.com>
2026-02-18 13:28:09 +0530

    2f argument

---
+++ 02_Basics/Print statement/.2f.py
@@ -0,0 +1,6 @@
+number = 1/3
+print(f"The number is {number:.2f}")
+
+
+# The format specifier .2f states that we want to display 2 decimals.
+# The letter f at the end means that we want the variable to be displayed as a float
