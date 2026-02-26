463244289b8c6f155201a28706f165b4e619411a
ankidvlpr <anki.dvlpr@gmail.com>
2026-02-26 12:46:24 +0530

    older year - 1979

---
+++ 03_Basics/Tuple/02_tuple.py
@@ -0,0 +1,28 @@
+# In this exercise we are handling tuples just like the ones described in the previous exercise.
+
+# Please write a function named older_people(people: list, year: int), 
+# which selects all those people on the list who were born before the year given as an argument. 
+# The function should return the names of these people in a new list.
+
+# An example of its use:
+
+# Sample output
+# [ 'Adam', 'Mary' ]
+
+def older_people(people: list, year: int):
+    new_list = [ ]
+    for person in people:
+        if person[1] < year:
+            new_list.append(person[0]) 
+
+    return new_list
+
+if __name__ == "__main__":
+    p1 = ("Adam", 1977)
+    p2 = ("Ellen", 1985)
+    p3 = ("Mary", 1953)
+    p4 = ("Ernest", 1997)
+    people = [p1, p2, p3, p4]
+
+    older = older_people(people, 1979)
+    print(older)
