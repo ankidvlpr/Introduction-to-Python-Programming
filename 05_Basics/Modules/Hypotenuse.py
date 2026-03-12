ff2b808dc0891ed7001a7fb6f1d0b7b48d137a98
ankidvlpr <anki.dvlpr@gmail.com>
2026-03-12 11:50:21 +0530

    Return : hypotenuse of a right triangle

---
+++ 05_Basics/Modules/Hypotenuse.py
@@ -0,0 +1,22 @@
+# Please write a function named hypotenuse(leg1: float, leg2: float),
+# which takes the lengths of the two sides adjacent to the right angle of an orthogonal triangle.
+# The function should return the length of the hypotenuse, or the side opposite to the right angle.
+
+# You can use the Pythagorean theorem to calculate the result. You will need the sqrt function from the math module.
+
+# Some examples:
+
+# print(hypotenuse(3,4)) # 5.0
+# print(hypotenuse(5,12)) # 13.0
+# print(hypotenuse(1,1)) # 1.4142135623730951
+
+
+from math import sqrt
+def hypotenuse(leg1: float, leg2: float):
+    """Calculate the hypotenuse using the Pythagorean theorem."""
+    c = sqrt(leg1**2 + leg2**2)
+    return c
+
+if __name__ == "__main__":
+    r = hypotenuse(3,4)
+    print(r)
